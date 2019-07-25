9a58f2d37ff7aedf1ac9c61c744e38bcde4db2dc
dataisland <dataisland@outlook.com>
2019-07-25 10:12:44 +0800

    Update utils.py

--- utils.py
+++ utils.py
@@ -4,30 +4,37 @@ import config
 from tensorflow.compat.v1 import placeholder, Variable, get_variable
 from tensorflow.contrib import slim
 
+
 def Input(input_shape):
     return placeholder(tf.float32, input_shape)
+
 
 def Conv2D(inputs, kernel=3, output_channel=64, stride=1):
     return slim.conv2d(inputs, output_channel, [kernel, kernel],
                        stride, 'SAME', data_format='NHWC', activation_fn=None,
                        weights_initializer=tf.contrib.layers.xavier_initializer())
-    
+
+
 def PReLU(inputs, alpha=0.2):
     pos = tf.nn.relu(inputs)
     neg = alpha * (inputs - abs(inputs)) * 0.5
 
     return pos + neg
 
+
 def LeakyReLU(inputs, alpha=0.2):
     return tf.nn.leaky_relu(inputs, alpha)
 
+
 def Sigmoid(inputs):
     return tf.nn.sigmoid(inputs)
+
 
 def BatchNormal(inputs, is_training=True):
     return slim.batch_norm(inputs, decay=0.9, epsilon=0.001,
                            updates_collections=tf.compat.v1.GraphKeys.UPDATE_OPS,
                            scale=False, fused=True, is_training=is_training)
+
 
 def phaseShift(inputs, scale, shape_1, shape_2):
     # Tackle the condition when the batch is None
@@ -35,6 +42,7 @@ def phaseShift(inputs, scale, shape_1, shape_2):
     X = tf.transpose(X, [0, 1, 3, 2, 4])
 
     return tf.reshape(X, shape_2)
+
 
 def PixelShuffler(inputs, scale=2):
     size = tf.shape(inputs)
@@ -56,14 +64,17 @@ def PixelShuffler(inputs, scale=2):
 
     return output
 
+
 def SubPixelConv2d(inputs, kernel=3, output_channel=256, stride=1):
     net = Conv2D(inputs, kernel, output_channel, stride)
     net = PixelShuffler(net, scale=2)
     net = PReLU(net)
     return net
 
+
 def Flatten(inputs):
     return slim.flatten(inputs)
+
 
 def Dense(inputs, output_size):
     return tf.layers.dense(inputs, output_size, activation=None,
