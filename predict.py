ec5fddad54b0f29e111c3755f3d04f591dbdde3a
dataisland <dataisland@outlook.com>
2019-08-07 11:26:05 +0800

    README

--- predict.py
+++ predict.py
@@ -6,20 +6,22 @@ import config
 import logging
 
 logging.disable(logging.WARNING)
+#os.environ["CUDA_VISIBLE_DEVICES"] = "-1"
 
 if __name__ == '__main__':
     with open(config.test_input, 'r') as f:
-        json = json.loads(f.read())
+        js = json.loads(f.read())
 
-    if 'input_path' in json:
+    if 'input_path' in js:
         config.test_image = []
-        for img in os.listdir(os.path.join(config.predict_path, json['input_path'])):
-            config.test_image.append(os.path.join(config.predict_path, json['input_path'], img))
-    if 'global_step' in json:
-        config.global_step = json['global_step']
+        for img in os.listdir(os.path.join(config.predict_path, js['input_path'])):
+            config.test_image.append(os.path.join(config.predict_path, js['input_path'], img))
+    if 'global_step' in js:
+        config.global_step = js['global_step']
 
     srgan = model.SRGAN(True)
     srgan.train(mode=1)
     srgan.Sess.close()
 
-    # TODO predict output.json
+    with open(config.test_output, 'w') as f:
+        json.dump({'output_path': os.path.join(config.predict_path, 'output')}, f)
